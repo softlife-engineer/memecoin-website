ed0123d0a46441cf30c7a3d1dda5a27fb08b2c95
Abonde Abdullahi Eniola <150375756+softlife-engineer@users.noreply.github.com>
2025-03-03 01:31:12 +0100

    Add files via upload

---
+++ RABID/RABID/coming soon/comming soon landing page with dark and light/js/main.js
@@ -0,0 +1,10 @@
+/* custom main js file made by jeandoe */
+
+const body = document.body;
+const themeBtn = document.getElementById('theme-btn');
+
+if(themeBtn){
+    themeBtn.addEventListener('click', () => {
+        body.classList.toggle('dark');
+    })
+}
